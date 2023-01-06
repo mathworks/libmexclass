55718894b605a0e0038bfbe454226ab52fb113b6
lafiona <fionala7@gmail.com>
2023-01-06 12:36:17 -0800

    Specify encoding conversion when receiving MATLAB string values. Export symbols on Windows to create mexclass.lib file.

--- example/CustomProxyFactory.cpp
+++ example/CustomProxyFactory.cpp
@@ -4,4 +4,6 @@
 
 std::shared_ptr<libmexclass::proxy::Proxy> CustomProxyFactory::make_proxy(const libmexclass::proxy::ClassName& class_name, const libmexclass::proxy::FunctionArguments& constructor_arguments) {
     registerProxy(CarProxy);
+    // TODO: Handle case where input class name does not match any of the registered proxy classes.
+    return nullptr;
 }
