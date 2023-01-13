f2c69a0f97fe18e6c761230f7e2005fe776b38b2
Jeremy Hughes <jhughes@mathworks.com>
2023-01-13 14:40:21 -0500

    Apply formatting

--- example/CustomProxyFactory.cpp
+++ example/CustomProxyFactory.cpp
@@ -2,7 +2,9 @@
 
 #include "CarProxy.h"
 
-std::shared_ptr<libmexclass::proxy::Proxy> CustomProxyFactory::make_proxy(const libmexclass::proxy::ClassName& class_name, const libmexclass::proxy::FunctionArguments& constructor_arguments) {
+std::shared_ptr<libmexclass::proxy::Proxy>
+CustomProxyFactory::make_proxy(const libmexclass::proxy::ClassName& class_name,
+                               const libmexclass::proxy::FunctionArguments& constructor_arguments) {
     registerProxy(CarProxy);
     // TODO: Handle case where input class name does not match any of the registered proxy classes.
     return nullptr;
