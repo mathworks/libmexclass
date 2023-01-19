39089bd34712a60a1276ccc0a5d8e9847642b4e1
Jeremy Hughes <jhughes@mathworks.com>
2023-01-19 11:14:00 -0500

    Add syntax for registering MATLAB class with different named  C++ Class proxy

--- example/CustomProxyFactory.cpp
+++ example/CustomProxyFactory.cpp
@@ -5,7 +5,7 @@
 std::shared_ptr<libmexclass::proxy::Proxy>
 CustomProxyFactory::make_proxy(const libmexclass::proxy::ClassName& class_name,
                                const libmexclass::proxy::FunctionArguments& constructor_arguments) {
-    registerProxy(CarProxy);
-    // TODO: Handle case where input class name does not match any of the registered proxy classes.
+                                
+    REGISTER_PROXY_SAME_NAME(CarProxy);
     return nullptr;
 }
