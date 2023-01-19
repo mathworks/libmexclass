e7b6f49ee97c1e09aee37777fb0cf9224fddd2b5
Jeremy Hughes <47334386+jhughes-mw@users.noreply.github.com>
2023-01-19 10:09:28 -0500

    12.clang format (#18)
    * Introduce clang format file
    
    * Update .clang-format for preferences
    
    * Apply formatting

--- example/Car.h
+++ example/Car.h
@@ -4,42 +4,42 @@
 
 class Car {
 
-    public:
+  public:
+    // Constructor
+    Car(const std::string& make, const std::string& model, const std::string& color)
+        : make{make}
+        , model{model}
+        , color{color} {}
 
-        // Constructor
-        Car(const std::string& make, const std::string& model, const std::string& color) : make{make}, model{model}, color{color} {}
+    // Actions
+    void Accelerate();
+    void Decelerate();
+    void Stop();
+    void Headlights(const bool state);
 
-        // Actions
-        void Accelerate();
-        void Decelerate();
-        void Stop();
-        void Headlights(const bool state);
+    // Setters
+    void SetSpeed(const std::uint64_t mph);
 
-        // Setters
-        void SetSpeed(const std::uint64_t mph);
+    // Getters
+    const std::uint64_t GetSpeed() const;
+    const std::string GetMake() const;
+    const std::string GetModel() const;
+    const std::string GetColor() const;
 
-        // Getters
-        const std::uint64_t GetSpeed() const;
-        const std::string GetMake() const;
-        const std::string GetModel() const;
-        const std::string GetColor() const;
-        
-        // Logging
-        void Print() const;
+    // Logging
+    void Print() const;
 
-    private:
+  private:
+    // Mutable state
+    std::uint64_t speed = 0;
+    bool headlights = false;
 
-        // Mutable state
-        std::uint64_t speed = 0;
-        bool headlights = false;
+    // Immutable state
+    const std::string make;
+    const std::string model;
+    const std::string color;
 
-        // Immutable state
-        const std::string make;
-        const std::string model;
-        const std::string color;
-
-        // Static Constants
-        const static inline std::uint64_t ACCELERATION_INCREMENT = 10;
-        const static inline std::uint64_t DECELERATION_INCREMENT = 5;
-
+    // Static Constants
+    const static inline std::uint64_t ACCELERATION_INCREMENT = 10;
+    const static inline std::uint64_t DECELERATION_INCREMENT = 5;
 };
--- example/CarProxy.h
+++ example/CarProxy.h
@@ -6,44 +6,46 @@
 #include "Car.h"
 
 class CarProxy : public libmexclass::proxy::Proxy {
-    public:
-        CarProxy(const libmexclass::proxy::FunctionArguments& constructor_arguments) : car{convert(constructor_arguments, 0), convert(constructor_arguments, 1), convert(constructor_arguments, 2)} {
-            // Step 1. Unpack constructor arguments.
+  public:
+    CarProxy(const libmexclass::proxy::FunctionArguments& constructor_arguments)
+        : car{convert(constructor_arguments, 0), convert(constructor_arguments, 1), convert(constructor_arguments, 2)} {
+        // Step 1. Unpack constructor arguments.
 
-            // Step 2. Initiliaze "raw" C++ object.
-            // car = Car(make, model, color);
+        // Step 2. Initiliaze "raw" C++ object.
+        // car = Car(make, model, color);
 
-            // Step 3. Register methods.
-            registerMethod(CarProxy, Accelerate);
-            registerMethod(CarProxy, Decelerate);
-            registerMethod(CarProxy, Stop);
-            registerMethod(CarProxy, Headlights);
-            registerMethod(CarProxy, SetSpeed);
-            registerMethod(CarProxy, GetSpeed);
-            registerMethod(CarProxy, GetMake);
-            registerMethod(CarProxy, GetModel);
-            registerMethod(CarProxy, GetColor);
-            registerMethod(CarProxy, Print);
-        }
-    private:
-        void Accelerate(libmexclass::proxy::method::Context& context);
-        void Decelerate(libmexclass::proxy::method::Context& context);
-        void Stop(libmexclass::proxy::method::Context& context);
-        void Headlights(libmexclass::proxy::method::Context& context);
-        void SetSpeed(libmexclass::proxy::method::Context& context);
-        void GetSpeed(libmexclass::proxy::method::Context& context);
-        void GetMake(libmexclass::proxy::method::Context& context);
-        void GetModel(libmexclass::proxy::method::Context& context);
-        void GetColor(libmexclass::proxy::method::Context& context);
-        void Print(libmexclass::proxy::method::Context& context);
+        // Step 3. Register methods.
+        registerMethod(CarProxy, Accelerate);
+        registerMethod(CarProxy, Decelerate);
+        registerMethod(CarProxy, Stop);
+        registerMethod(CarProxy, Headlights);
+        registerMethod(CarProxy, SetSpeed);
+        registerMethod(CarProxy, GetSpeed);
+        registerMethod(CarProxy, GetMake);
+        registerMethod(CarProxy, GetModel);
+        registerMethod(CarProxy, GetColor);
+        registerMethod(CarProxy, Print);
+    }
 
-        std::string convert(const libmexclass::proxy::FunctionArguments& constructor_arguments, std::uint64_t index) const {
-            matlab::data::StringArray mda = constructor_arguments[index];
-            const std::string result = std::string(mda[0]);
-            return result;
-        }
+  private:
+    void Accelerate(libmexclass::proxy::method::Context& context);
+    void Decelerate(libmexclass::proxy::method::Context& context);
+    void Stop(libmexclass::proxy::method::Context& context);
+    void Headlights(libmexclass::proxy::method::Context& context);
+    void SetSpeed(libmexclass::proxy::method::Context& context);
+    void GetSpeed(libmexclass::proxy::method::Context& context);
+    void GetMake(libmexclass::proxy::method::Context& context);
+    void GetModel(libmexclass::proxy::method::Context& context);
+    void GetColor(libmexclass::proxy::method::Context& context);
+    void Print(libmexclass::proxy::method::Context& context);
 
-        // Note: Clients can replace this with whatever "raw" C++ Class they want
-        // to proxy method calls to.
-        Car car;
+    std::string convert(const libmexclass::proxy::FunctionArguments& constructor_arguments, std::uint64_t index) const {
+        matlab::data::StringArray mda = constructor_arguments[index];
+        const std::string result = std::string(mda[0]);
+        return result;
+    }
+
+    // Note: Clients can replace this with whatever "raw" C++ Class they want
+    // to proxy method calls to.
+    Car car;
 };
