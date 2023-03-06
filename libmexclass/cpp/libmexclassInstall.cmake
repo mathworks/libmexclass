5c0e3ac0c0f689b8e806322f8cb0de4533581e99
shegden <shegden@mathworks.com>
2023-03-06 14:00:23 -0500

    Updated install script to use MATLAB source from downloaded libmexlass. Also, removing copying of packaged install to client's target dir.

--- libmexclass/cpp/libmexclassInstall.cmake
+++ libmexclass/cpp/libmexclassInstall.cmake
@@ -1,17 +1,16 @@
 message(STATUS "Executing LIBMEXCLASS_INSTALL_STEPS_FILE: ${CMAKE_CURRENT_LIST_FILE} using INSTALL_COMMAND...")
 
-# ${CMAKE_CURRENT_LIST_DIR} points to the folder in which current executing script is located.
-# Current script LIBMEXCLASS_INSTALL_STEPS_FILE is in the libmexclass/cpp folder.
-set(LIBMEXCLASS_MATLAB_SOURCES "${CMAKE_CURRENT_LIST_DIR}/../matlab/+libmexclass/")
+set(LIBMEXCLASS_MATLAB_SOURCES "${CMAKE_CURRENT_BINARY_DIR}/../libmexclass/libmexclass/matlab/+libmexclass/")
 message(STATUS "LIBMEXCLASS_MATLAB_SOURCES: ${LIBMEXCLASS_MATLAB_SOURCES}")
 
-# Copy ${LIBMEXCLASS_MATLAB_SOURCES} first so that ${CMAKE_CURRENT_BINARY_DIR}/+libmexclass/+proxy/ folder is there to which we copy necessary binaries later.
+# Copy ${LIBMEXCLASS_MATLAB_SOURCES} first so that ${CMAKE_CURRENT_BINARY_DIR}/+libmexclass/+proxy/ folder is there to which we copy necessary binaries.
 set(CMAKE_PACKAGED_LIBMEXCLASS_INSTALL_DIR "${CMAKE_CURRENT_BINARY_DIR}/+libmexclass")
 message(STATUS "CMAKE_PACKAGED_LIBMEXCLASS_INSTALL_DIR: ${CMAKE_PACKAGED_LIBMEXCLASS_INSTALL_DIR}")
 message(STATUS "Copying LIBMEXCLASS_MATLAB_SOURCES to packaged folder CMAKE_PACKAGED_LIBMEXCLASS_INSTALL_DIR...")
 file(COPY ${LIBMEXCLASS_MATLAB_SOURCES} DESTINATION ${CMAKE_PACKAGED_LIBMEXCLASS_INSTALL_DIR})
 
 set(ACTUAL_BINARY_DIR ${CMAKE_CURRENT_BINARY_DIR})
+# TODO: Get the binary names and extensions using Matlab_MEX_EXTENSION in CMake FindMatlab.
 if(WIN32)
 	# On Windows, the Visual Studio build adds an additional "Release" folder to place the output binaries.
 	set(ACTUAL_BINARY_DIR "${ACTUAL_BINARY_DIR}/Release/")
@@ -26,6 +25,3 @@ message(STATUS "ACTUAL_BINARY_DIR: ${ACTUAL_BINARY_DIR}")
 # Copy the binaries to ${CMAKE_CURRENT_BINARY_DIR}/+libmexclass/+proxy/ packaged folder.
 message(STATUS "Copying libmexclass binaries from ACTUAL_BINARY_DIR to packaged folder CMAKE_PACKAGED_LIBMEXCLASS_INSTALL_DIR...")
 file(COPY ${INSTALL_BINARIES_LIST} DESTINATION ${CMAKE_PACKAGED_LIBMEXCLASS_INSTALL_DIR}/+proxy/)
-
-message(STATUS "Copying CMAKE_PACKAGED_LIBMEXCLASS_INSTALL_DIR to client's CMAKE_INSTALL_DIR: ${CMAKE_INSTALL_DIR}...")
-file(COPY ${CMAKE_PACKAGED_LIBMEXCLASS_INSTALL_DIR} DESTINATION ${CMAKE_INSTALL_DIR}/)
