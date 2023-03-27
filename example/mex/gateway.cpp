e53ef993b74869c3d6bb558c4b26b116aec2a383
Kevin Gurney <kgurney@mathworks.com>
2023-03-27 15:15:20 -0400

    Add basic debug output to MEX gatewy.

--- example/mex/gateway.cpp
+++ example/mex/gateway.cpp
@@ -1,9 +1,11 @@
 #include "mex.hpp"
 #include "mexAdapter.hpp"
 
+#include <iostream>
+
 class MexFunction : public matlab::mex::Function {
     public:
         void operator()(matlab::mex::ArgumentList outputs, matlab::mex::ArgumentList inputs) {
-
+            std::cout << "test" << std::endl;
         }
 };
