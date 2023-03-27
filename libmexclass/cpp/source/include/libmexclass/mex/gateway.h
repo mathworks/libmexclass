66982a83d55a92faa86c4598b2c38f904d7a8f44
Kevin Gurney <kgurney@mathworks.com>
2023-03-27 16:52:39 -0400

    Add basic gateway template function.

---
+++ libmexclass/cpp/source/include/libmexclass/mex/gateway.h
@@ -0,0 +1,72 @@
+#pragma once
+
+#include "libmexclass/action/Action.h"
+#include "libmexclass/action/Factory.h"
+#include "libmexclass/mex/Arguments.h"
+#include "libmexclass/mex/Matlab.h"
+#include "libmexclass/mex/State.h"
+#include "libmexclass/proxy/Factory.h"
+
+#include "mex.hpp"
+#include "mexAdapter.hpp"
+
+namespace libmexclass::mex {
+// =======================================
+// MEX Gateway Function Supported Syntaxes
+// =======================================
+//
+// --------------------------------------------------------------------
+//
+// ====================
+// action::Create
+// ====================
+//
+// **NOTE** <proxy-constructor-args should be a cell array.
+//
+// >> <proxy-id> = libmexclass.proxy.gateway("Create", <proxy-name>, <proxy-constructor-args>)
+//
+// --------------------------------------------------------------------
+//
+// ====================
+// action::MethodCall
+// ====================
+//
+// **NOTE** <method-args> should be a cell array.
+//
+// >> <proxy-method-outputs> = libmexclass.proxy.gateway("MethodCall", <proxy-id>, <proxy-method-name>, <proxy-method-args>)
+//
+// --------------------------------------------------------------------
+// 
+// ====================
+// action::Destroy 
+// ====================
+//
+// >> <proxy-destroy-status> = libmexclass.proxy.gateway("Destroy", "<proxy-id>")
+//
+// ==================
+
+// This is the main MEX function gateway.
+// This uses a rough approximation of the "Proxy" Design Pattern to handle dispatching to appropriate object method calls.
+    template<typename ProxyFactory>
+    inline void gateway(matlab::mex::Arguments inputs, matlab::mex::Arguments outputs, Matlab matlab) {
+        // Wrap the inputs and outputs.
+        mex::Arguments output_arguments{outputs.begin()};
+        mex::Arguments input_arguments{inputs.begin()};
+
+        // Store the inputs, outputs, and MATLAB Engine pointer in a libmexclass::mex::State instance.
+        mex::State state{input_arguments, output_arguments, matlab};
+
+        // Create a polymorphic proxy::Factory instance.
+        // NOTE: Clients provide the implementation for CustomProxyFactory.
+        std::shared_ptr<proxy::Factory> proxy_factory = std::make_shared<CUSTOM_PROXY_FACTORY_CLASS_NAME>();
+
+        // Create an action::Factory.
+        action::Factory action_factory{proxy_factory};
+
+        // Create an Action.
+        std::unique_ptr<action::Action> action = action_factory.makeAction(state);
+        
+        // Execute the Action.
+        action->execute();
+    }
+}
