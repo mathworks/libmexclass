53c998be6a115d6c0c2e2f4b5b729253a3f63316
Kevin Gurney <kgurney@mathworks.com>
2023-03-27 15:09:36 -0400

    Add basic MEX gateway.

---
+++ example/mex/gateway.cpp
@@ -0,0 +1,9 @@
+#include "mex.hpp"
+#include "mexAdapter.hpp"
+
+class MexFunction : public matlab::mex::Function {
+    public:
+        void operator()(matlab::mex::ArgumentList outputs, matlab::mex::ArgumentList inputs) {
+
+        }
+};
