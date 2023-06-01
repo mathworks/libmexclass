6a0cf304eaed6fb990602a148ace4a39effaf5d8
Sarah Gilmore <sgilmore@mathworks.com>
2023-06-01 09:21:22 -0400

    Add constructor to Error struct

--- libmexclass/cpp/source/include/libmexclass/error/Error.h
+++ libmexclass/cpp/source/include/libmexclass/error/Error.h
@@ -6,6 +6,10 @@ namespace libmexclass::error {
     struct Error {
         const std::string id;
         const std::string message;
+        
+        Error(const std::string& error_id, const std::string& error_message)
+            : id{error_id}
+            , message{error_message} {}
     };
 
     class ErrorBuilder {
