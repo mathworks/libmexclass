072edbe83c11592ac32d2920a23f0986758a085f
Sarah Gilmore <sgilmore@mathworks.com>
2023-06-29 15:33:51 -0400

    Fix compiler error

--- libmexclass/cpp/source/libmexclass/proxy/ProxyManager.cpp
+++ libmexclass/cpp/source/libmexclass/proxy/ProxyManager.cpp
@@ -14,11 +14,11 @@ void ProxyManager::unmanageProxy(ID id) {
 
 ProxyResult ProxyManager::getProxy(ID id) {
     auto proxy_key_value = ProxyManager::singleton.proxy_map.find(id);
-    if (proxy_key_value->first != ProxyManager::singleton.proxy_map.end()) {
-        return proxy_key_value->second;
-    } else {
+    if (proxy_key_value->first == ProxyManager::singleton.proxy_map.end()) {
         const std::string msg = "Unknown Proxy ID: " + std::to_string(id);
         return libmexclass::error::Error{"libmexclass:proxy:UnknownID", msg};
+    } else {
+        return proxy_key_value->second;
     }
 }
 
